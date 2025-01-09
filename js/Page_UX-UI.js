f70d18d6117442cdb0703e0c0d52e6c8cdfd582a
BZei <165918405+BZei@users.noreply.github.com>
2025-01-09 16:57:10 +0100

    Add files via upload

---
+++ js/Page_UX-UI.js
@@ -0,0 +1,80 @@
+let currentImageIndex = 0; // Index de l'image courante
+
+function openModal(images, description) {
+  // Affiche la modale
+  document.getElementById('modal').style.display = 'block';
+  
+  // Met à jour la description
+  document.getElementById('modal-description').innerText = description;
+  
+  // Met à jour la première image
+  changeImage(images[currentImageIndex]);
+
+  // Stocke les images à afficher
+  window.images = images;
+}
+
+function changeImage(imageSrc) {
+  document.getElementById('modal-img').src = imageSrc;
+}
+
+function nextImage() {
+  // Passer à l'image suivante
+  if (currentImageIndex < window.images.length - 1) {
+    currentImageIndex++;
+    changeImage(window.images[currentImageIndex]);
+  }
+}
+
+function prevImage() {
+  // Retourner à l'image précédente
+  if (currentImageIndex > 0) {
+    currentImageIndex--;
+    changeImage(window.images[currentImageIndex]);
+  }
+}
+
+
+
+
+
+function closeModal() {
+  // Ferme la modale
+  document.getElementById('modal').style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Ouvrir la pop-up lorsqu'on clique sur "A propos de moi"
+document.getElementById('about-me-btn').addEventListener('click', function() {
+    document.getElementById('about-me-popup').style.display = 'block';
+  });
+  
+  // Fermer la pop-up lorsqu'on clique sur le bouton de fermeture
+  document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('about-me-popup').style.display = 'none';
+  });
